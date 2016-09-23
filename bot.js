828e3e58424cf6ae01a1a9a8dbdd4dcccb78a705
Niels Bantilan <niels@peggedsoftware.com>
2016-09-23 00:29:34 -0400

    polished deployment system, added logging, reading in data
    - `fibi_decision_tree.json` is fixture data for testing the
      basic flow of the chat logic

--- bot.js
+++ bot.js
@@ -2,7 +2,15 @@
 
 const botBuilder = require('claudia-bot-builder');
 const excuse = require('huh');
+const fs = require('fs');
+console.log("Trying to parse data");
+var dtree = require('./data/fibi_decision_tree.json');
 
-module.exports = botBuilder(request =>
-  `Thank you for sending ${request.text}. Your message is very important to us. The problem was caused by ${excuse.get()}`
-);
+module.exports = botBuilder(function (message, request) {
+  console.log("This is the message", message);
+  console.log("This is the request", request);
+  console.log(dtree);
+  return 'Thanks for sending ' + message.text  + 
+      '. Your message is very important to us, but ' + 
+      excuse.get() + '. This is production.' + dtree.data[0].copy;
+});
