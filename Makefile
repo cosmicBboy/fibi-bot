d00492a2dd0634556d57a685c21ede8706cff2a3
cosmicBboy <niels.bantilan@gmail.com>
2016-10-07 01:28:14 -0400

    added makefile targets to create menu, change bot templates
    - using text and quickreply buttons
    - made changes to logic to handle postback and quick_reply
      responses

--- Makefile
+++ Makefile
@@ -2,6 +2,29 @@
 
 AWS_DEFAULT_PROFILE=fibidev
 
-.PHONY: create-data
-create-data:
-	# TODO: call java script to convert Google Spreadsheet to .json
+.PHONY: set-timeout
+set-timout:
+	aws --profile $(AWS_DEFAULT_PROFILE) lambda update-function-configuration --function-name fibi --timeout $(T)
+
+.PHONY: add-greeting
+
+.PHONY: add-menu
+add-menu:
+	curl -X POST -H "Content-Type: application/json" -d '{ \
+	  "setting_type" : "call_to_actions", \
+	  "thread_state" : "existing_thread", \
+	  "call_to_actions":[ \
+	    { \
+	      "type":"postback", \
+	      "title":"Start over", \
+	      "payload":"START_OVER" \
+	    } \
+	  ] \
+	}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$(PAGE_ACCESS_TOKEN)" \
+
+.PHONY: rm-menu
+rm-menu:
+	curl -X DELETE -H "Content-Type: application/json" -d '{ \
+	  "setting_type":"call_to_actions", \
+	  "thread_state":"existing_thread" \
+	}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$(PAGE_ACCESS_TOKEN)"
--- bot.js
+++ bot.js
@@ -13,11 +13,10 @@ function mainMenu() {
   // TODO: The logic for figuring out with question is next should be done
   // here by finding the pointsTo field in each of the inputs. In the case
   // below, the .addButton calls contain the data for `input` types.
-  return new fbTemplate.generic()
-    .addBubble(`What kind of support are you interested?`)
-      .addButton("legal status", "1 question")
-      .addButton("driver's license", "1 question")
-      .addButton("scholarships", "54 question")
+  return new fbTemplate.text(`What kind of support are you interested?`)
+      .addQuickReply("legal status", "1 question")
+      .addQuickReply("driver's license", "1 question")
+      .addQuickReply("scholarships", "54 question")
     .get();
 }
 
@@ -33,8 +32,7 @@ function questionTemplate(questionObj) {
   if (inputs[0].type === "END") {
     return inputs[0].copy;
   } else {
-    var message = new fbTemplate.generic()
-      .addBubble(questionObj.copy);
+    var message = new fbTemplate.text(questionObj.copy);
 
     _.each(inputs, input => {
       // Assume that input can only point to one question
@@ -48,7 +46,7 @@ function questionTemplate(questionObj) {
       console.log("This is a single input:", input);
       console.log("Copy:", input.copy);
       console.log("Request text:", text);
-      message.addButton(input.copy, text);
+      message.addQuickReply(input.copy, text);
     });
 
     // If object contains blurb, return a list of [blurb, message]
@@ -65,8 +63,24 @@ const api = botBuilder(function (request, originalApiRequest) {
   console.log("This is the request", JSON.stringify(request));
   console.log(
     "This is the original api request", JSON.stringify(originalApiRequest));
+  originalApiRequest.lambdaContext.callbackWaitsForEmptyEventLoop = false;
 
-  if (!request.postback) {
+  var message = request.originalRequest.message,
+      quickReply = undefined,
+      payload;
+
+  // if message is defined, then try to grab the quick_reply object
+  if (!!message) {
+    console.log("Assigning quickReply:", request.originalRequest.message);
+    quickReply = message.quick_reply;
+  }
+
+  console.log("quick reply:", quickReply);
+  console.log("START_OVER", request.text == "START_OVER");
+  console.log("quick reply?", !quickReply);
+
+  if (request.text == "START_OVER" || !quickReply &&
+      !request.postback) {
     return rp.get(`https://graph.facebook.com/v2.6/${request.sender}?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=${originalApiRequest.env.facebookAccessToken}`)
       .then(response => {
         console.log("This is the response", JSON.stringify(response));
@@ -80,8 +94,18 @@ const api = botBuilder(function (request, originalApiRequest) {
       });
   }
 
-  if (_.contains(request.text.split(' '), "END")) {
-    var split =  request.text.split(' ');
+  if (!!quickReply) {
+    payload = quickReply.payload;
+  } else if (!quickReply && request.postback) {
+    payload = request.text;
+  } else {
+    throw "no payload!";
+  }
+
+  console.log("This is the payload", payload);
+
+  if (_.contains(payload.split(' '), "END")) {
+    var split =  payload.split(' ');
     var endObjectId = split[0];
     var endObject = _.find(data, o => {
       return o.id === endObjectId;
@@ -90,8 +114,8 @@ const api = botBuilder(function (request, originalApiRequest) {
     return endObject.copy;
   }
 
-  if (_.contains(request.text.split(' '), "question")) {
-    var split =  request.text.split(' ');
+  if (_.contains(payload.split(' '), "question")) {
+    var split =  payload.split(' ');
     var nextId = split[0];
     var nextQuestion = _.find(data, o => {
       return o.id === nextId;
@@ -100,8 +124,8 @@ const api = botBuilder(function (request, originalApiRequest) {
     return questionTemplate(nextQuestion);
   }
 
-  if (_.contains(request.text.split(' '), "link")) {
-    var split =  request.text.split(' ');
+  if (_.contains(payload.split(' '), "link")) {
+    var split =  payload.split(' ');
     var linkId = split[0];
     var link = _.find(data, o => {
       return o.id === linkId;
